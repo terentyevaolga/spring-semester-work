7a7c88170fb2ee2257d3c1ff71871a4f79311dd1
terentyevaolga <terentyeevaolga@gmail.com>
2021-11-01 23:42:07 +0300

    old version

--- src/main/webapp/javascript/modalWindow.js
+++ src/main/webapp/javascript/modalWindow.js
@@ -1,25 +1,25 @@
-const openPopUp = document.getElementById('open_pop_up');
-const closePopUp = document.getElementById('pop_up_close');
-const popUp = document.getElementById('pop_up');
-
-openPopUp.addEventListener('click', function (e) {
-    e.preventDefault();
-    popUp.classList.add('active');
-});
-
-closePopUp.addEventListener('click', ()=> {
-    popUp.classList.remove('active')
-});
-
-const openPopIn = document.getElementById('open_pop_in');
-const closePopIn = document.getElementById('pop_in_close');
-const popIn = document.getElementById('pop_in');
-
-openPopIn.addEventListener('click', function (e) {
-    e.preventDefault();
-    popIn.classList.add('active');
-});
-
-closePopIn.addEventListener('click', ()=> {
-    popIn.classList.remove('active')
-});
+// const openPopUp = document.getElementById('open_pop_up');
+// const closePopUp = document.getElementById('pop_up_close');
+// const popUp = document.getElementById('pop_up');
+//
+// openPopUp.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     popUp.classList.add('active');
+// });
+//
+// closePopUp.addEventListener('click', ()=> {
+//     popUp.classList.remove('active')
+// });
+//
+// const openPopIn = document.getElementById('open_pop_in');
+// const closePopIn = document.getElementById('pop_in_close');
+// const popIn = document.getElementById('pop_in');
+//
+// openPopIn.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     popIn.classList.add('active');
+// });
+//
+// closePopIn.addEventListener('click', ()=> {
+//     popIn.classList.remove('active')
+// });
